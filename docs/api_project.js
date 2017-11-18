a411dd464800a58baa0eb5500360c67de56f36e3
jiangxb <771769087@qq.com>
2017-11-18 22:57:05 +0800

    add doc

---
+++ docs/api_project.js
@@ -0,0 +1,28 @@
+define({
+  "name": "mitures-server3 api",
+  "version": "1.0.0",
+  "description": "mitures-server3 api",
+  "title": "mitures-server3 api",
+  "url": "10.0.0.61:8000",
+  "sampleUrl": "http://10.0.0.61:8000",
+  "header": {
+    "title": "基本说明",
+    "content": "<h1>apidoc 配置文件介绍</h1>\n<pre><code>apidoc.json 是apidoc 的配置文件\nversion:&quot;1.0.0&quot;  版本管理\nurl:&quot;10.0.0.245:8000&quot; 是配置测试主机地址, 本机测试请输入自己地址\nheader.md　文件添加页首描述\nfooter.md　文件添加页尾描述\n\n执行命令: apidoc -i ../java -o api\n</code></pre>\n"
+  },
+  "footer": {
+    "title": "页脚",
+    "content": ""
+  },
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-11-18T05:17:40.152Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
